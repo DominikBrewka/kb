1fd252eee868cbbecd1775344a245f57bfb60ea1
yakkudev <dominik.brewka@gmail.com>
2023-06-01 08:34:13 +0200

    zmiany zmiany zmiany

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 // vite.config.js
 /** @type {import('vite').UserConfig} */
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
 export default {
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            rodzaje: resolve(__dirname, 'rodzaje.html'),
+            personalizacja: resolve(__dirname, 'personalizacja.html'),
+            extra: resolve(__dirname, 'extra.html'),
+          },
+        },
+      },
     // config options
     server: {
         port: 3000
